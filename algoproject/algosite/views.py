7e4e11695844329e7329e5b570eb923f195ad2bf
nikw <nikw2008@mail.ru>
2025-06-10 10:27:38 +0300

    отступы

--- algoproject/algosite/views.py
+++ algoproject/algosite/views.py
@@ -64,14 +64,14 @@ def topic_requests(request):
     for topic_request in topic_requests_list:
         if topic_request.explanation:
             # Создаем объект BytesIO из бинарных данных
-            docx_file = BytesIO(topic_request.explanation)
-            result = mammoth.convert_to_html(docx_file)
+            #docx_file = BytesIO(topic_request.explanation)
+            result = docx_bin_to_html(topic_request.explanation)
             explanation_html_dict[topic_request.id] = result.value
 
         if topic_request.example_code:
             # Аналогично для example_code
-            docx_file = BytesIO(topic_request.example_code)
-            result = mammoth.convert_to_html(docx_file)
+            #docx_file = BytesIO(topic_request.example_code)
+            result = docx_bin_to_html(topic_request.example_code)
             example_code_html_dict[topic_request.id] = result.value
 
     return render(request, 'algosite/topic_requests.html', {
